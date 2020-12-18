7973b1dcd8ddc342bfd72ec6f4eaf48e95b1d943
Jerud-Moyer <jerudmoyer@gmail.com>
2020-12-18 08:57:26 -0800

    ok

--- src/components/amphead/Experiment.jsx
+++ src/components/amphead/Experiment.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import { Parallax } from 'react-scroll-parallax'
-import styles from './Experiment.css'
-import ampheadLogo from '../assets/amphead-parallax.png'
-import ampRepair from '../assets/amp-repair-parallax.png'
+import React from 'react';
+import { Parallax } from 'react-scroll-parallax';
+import styles from './Experiment.css';
+import ampheadLogo from '../assets/amphead-parallax.png';
+import ampRepair from '../assets/amp-repair-parallax.png';
 
 const Experiment = () => {
   return (
 
     <div className={styles.root}>
-    <div className={styles.container}>
-    <Parallax className={styles.logo}  x={[-40, 20]} tagOuter="figure"
-    offsetYMax={150}
-    offsetYMin={-50}
-    offsetXMax={-40}
-    offsetXMin={20}
-    >
-        <img src={ampheadLogo} alt='ok' />
+      <div className={styles.container}>
+        <Parallax className={styles.logo}  x={[-40, 20]} tagOuter="figure"
+          offsetYMax={150}
+          offsetYMin={-50}
+          offsetXMax={-40}
+          offsetXMin={20}
+        >
+          <img src={ampheadLogo} alt="ok" />
        
-    </Parallax>
-    <Parallax className={styles.section}  x={[125, -20]} tagOuter="figure"
-    offsetYMax={150}
-    offsetYMin={-50}
-    offsetXMax={40}
-    offsetXMin={-20}
-    >
-        <img src={ampRepair} alt='ok' />
-    </Parallax>
+        </Parallax>
+        <Parallax className={styles.section}  x={[125, -20]} tagOuter="figure"
+          offsetYMax={150}
+          offsetYMin={-50}
+          offsetXMax={40}
+          offsetXMin={-20}
+        >
+          <img src={ampRepair} alt="ok" />
+        </Parallax>
+      </div>
     </div>
-    </div>
-);
+  );
 
-}
+};
 
-export default Experiment
+export default Experiment;
--- src/components/amphead/News.jsx
+++ src/components/amphead/News.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAnnouncements } from '../../actions/announcementActions';
+import { selectAnnouncements } from '../../selectors/ampheadSelectors';
+
 import BigLogo from './BigLogo';
 import styles from './News.css';
 
 const News = () => {
+  const announcements = useSelector(selectAnnouncements);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAnnouncements());
+  }, []);
+
+  const announcementElements = announcements.filter(announcement => (
+    announcement.side === 'amphead')).map(announcement => (
+    <li key={announcement.id} className={styles.announcement} >
+      <h2>{announcement.title}</h2>
+      <p>{announcement.body}</p>
+    </li>
+  ));
 
   return (
     <div className={styles.bigBox}>
@@ -10,13 +28,16 @@ const News = () => {
         <BigLogo />
       </div>
       <div className={styles.infoBox}>
-        <h1>Providing Amplifier Repair and Maintenance to Portland Area Musicians since 1996</h1>
-        <h3>shop hours by appointment</h3>
-        <h4>Monday through Friday 10am - 6pm and Saturday 10am - 2pm</h4>
-        <h3>we have new procedures for social-distancing, plaease contact to schedule a visit.</h3>
+        <h1>
+          Providing Amplifier Repair and Maintenance to Portland
+           Area Musicians since 1996
+        </h1>
         <h3>503 231-4035</h3>
         <h3>5225-a SE 78th ave. Portland, OR 97206</h3>
       </div>
+      <ul className={styles.announcementBox} >
+        {announcementElements}
+      </ul>
     </div>
   );
 };
