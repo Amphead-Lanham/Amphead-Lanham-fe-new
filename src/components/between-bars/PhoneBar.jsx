5bc9403d1a980a4dec32c1a40ca513690d757247
Jerud-Moyer <jerudmoyer@gmail.com>
2021-01-05 14:44:36 -0800

    add phone bars

---
+++ src/components/between-bars/PhoneBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ampheadLogo from '../../assets/amphead-parallax.png';
+import styles from './PhoneBar.css';
+
+function PhoneBar({ section }) {
+  return (
+    <div className={styles.bar}>
+      <img className={styles.logo} src={ampheadLogo}/>
+      <h1 className={styles.section} >{section}</h1>
+    </div>
+  );
+}
+
+PhoneBar.propTypes = {
+  section: PropTypes.string.isRequired
+};
+
+export default PhoneBar;
