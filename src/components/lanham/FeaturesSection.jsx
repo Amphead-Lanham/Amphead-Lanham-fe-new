f7bd4b94ee62d43e480ec87abd7b104bfc3e5e8d
Jerud-Moyer <jerudmoyer@gmail.com>
2023-03-02 21:01:03 -0800

    start with carousel controls

--- src/components/lanham/FeaturesSection.jsx
+++ src/components/lanham/FeaturesSection.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { features } from '../../data/amp-features.js';
 import { useInView } from 'react-intersection-observer';
 import styles from './FeaturesSection.css';
 
 const FeaturesSection = () => {
+  const aboutRef = useRef(null);
 
-  const [ref, inView] = useInView({
-    triggerOnce: true,
+  useEffect(() => {
+  }, [aboutRef]);
+  
+  const { ref, inView } = useInView({
+    // triggerOnce: true,
     threshold: .3,
+    onChange: () => {
+      const scroller = () => {
+        const yAxis = aboutRef.current.offsetTop;
+        console.log('here?  ', yAxis);
+      };
+      if(inView) {
+        console.log('HELLO?');
+        window.addEventListener('scroll', scroller, { passive: true });
+      } 
+      if(!inView) {
+        console.log('bye bye');
+        window.removeEventListener('scroll', scroller, false);
+      }
+    }
   });
 
   const featuresItems = features.map((item, index) => (
@@ -17,24 +35,27 @@ const FeaturesSection = () => {
   ));
 
   return (
-    <div
-      className={inView ? styles.featureBox : styles.placeHolderBox}
-      ref={ref}
-      style={{
-        transition: 'ease-in',
-        visibility: inView ? 'visible' : 'hidden'
-      }}>
-      <ul className={inView ? styles.featureList : null}
+    <div ref={aboutRef}>
+      <div
+        className={inView ? styles.featureBox : styles.placeHolderBox}
         ref={ref}
+        style={{
+          transition: 'ease-in',
+          visibility: inView ? 'visible' : 'hidden'
+        }}
       >
-        {featuresItems}
-      </ul>
-      <h1
-        className={inView ? styles.promise : null}
-        ref={ref}
-      >
+        <ul className={inView ? styles.featureList : null}
+          ref={ref}
+        >
+          {featuresItems}
+        </ul>
+        <h1
+          className={inView ? styles.promise : null}
+          ref={ref}
+        >
         Our Promise is Guaranteed Quality
-      </h1>
+        </h1>
+      </div>
     </div>
   );
 };
