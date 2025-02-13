15ba60d91ff9f2b4bc0e2b37391d3e54359d29c1
Jerud-Moyer <jerudmoyer@gmail.com>
2025-02-13 09:20:55 -0800

    update auth requsets

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,21 @@
 import { get, post } from './request';
 
 export const postSignup = (email, password, firstName) =>
-  post('/signup', { email, password, firstName });
+  post('/signup', { 
+    email, 
+    password, 
+    firstName,
+    client: 'amphead', 
+    cookiePlease: true 
+  });
 
 export const postLogin = (email, password) =>
-  post('/login', { email, password });
+  post('/login', { 
+    email, 
+    password,
+    client: 'amphead', 
+    cookiePlease: true 
+  })
 
 export const getVerify = () =>
   get('/verify');
