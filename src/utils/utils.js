ae8fac25bb4225cbb0667408cc17037eaad42024
Jerud Moyer <jerudmoyer@gmail.com>
2025-05-01 15:34:26 -0700

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,6 @@
 export const formatAnnouncementBody = (string) => {
   // return string.replaceAll('.', '.  -farts-')
   //   .replaceAll('<line-break>', '\n');
+  // staging change
   return string.replaceAll('---', '\r\n').replaceAll('.', '.\n');
 };
